56161eac92d82c6a5723026e340f1c9a1c210248
Serik Makzhanov <Serik.Makzhanov@gmail.com>
2022-12-05 10:04:03 +0600

    Stage 2/6 complite

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,16 +1,38 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        startMachine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write how many cups of coffee you will need:\n> ");
+        int cupsOfCoffee = scanner.nextInt();
+        System.out.printf("For %d cups of coffee you will need:\n", cupsOfCoffee);
+        System.out.println(calculationForOneCupOfCoffee(cupsOfCoffee));
+        /*startMachine();
         grindBeans();
         boilWater();
         mixWaterAndBeans();
         pourCoffee();
         pourMilk();
-        serveCoffee();
+        serveCoffee();*/
     }
 
+    /**
+     * This metod figure out how much of each ingredient the machine will need
+     * @param cupsOfCoffee - numbers of coffee drinks
+     * @return returns String with list of the required ingredient amounts
+     */
+    public static String calculationForOneCupOfCoffee(int cupsOfCoffee) {
+        int waterPerCup = 200;
+        int milkPerCup = 50;
+        int coffeeBeansPerCup = 15;
+        int waterForCups, milkForCups, coffeeBeansForCups;
+        waterForCups = waterPerCup * cupsOfCoffee;
+        milkForCups = milkPerCup * cupsOfCoffee;
+        coffeeBeansForCups = coffeeBeansPerCup * cupsOfCoffee;
+        return waterForCups + " ml of water\n" + milkForCups + " ml of milk\n" + coffeeBeansForCups + " g of coffee beans";
+    }
     private static void startMachine() {
         System.out.println("Starting to make a coffee");
     }
