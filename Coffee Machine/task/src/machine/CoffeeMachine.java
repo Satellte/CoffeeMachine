e3e73862d8e8837fcf25c5f150a666b19ad8dd97
Serik Makzhanov <Serik.Makzhanov@gmail.com>
2022-12-16 14:58:46 +0600

    Project complete

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -6,7 +6,6 @@ static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         DeLonghi DeLonghi = new DeLonghi();
         DeLonghi.setResources(400, 540, 120, 9, 550);
-
         machine.DeLonghi.getMenu();
     }
 }
